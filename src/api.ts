f778daf9a5513af423a4216d8db34665b145fb4e
Sören <hello@soerenschwert.de>
2024-03-20 14:45:03 +0100

    Accept a custom fetch implementation for API calls

--- src/api.ts
+++ src/api.ts
@@ -34,12 +34,14 @@ import {
 
 let API_URL: string | undefined;
 let API_KEY: string | undefined;
+let FETCH: typeof fetch;
 // let REFERRAL: ReferralCodes | undefined;
 
-export function init(url: string, key: string /* , referral?: ReferralCodes */) {
+export function init(url: string, key: string, options?: Partial<{ /* referral?: ReferralCodes, */customFetch? : typeof fetch}>) {
     if (!url || !key) throw new Error('url and key must be provided');
     API_URL = url;
     API_KEY = key;
+    FETCH = options?.customFetch || fetch;
     // REFERRAL = referral;
 }
 
@@ -53,7 +55,7 @@ async function api(
 ): Promise<FastspotResult> {
     if (!API_URL || !API_KEY) throw new Error('API URL and key not set, call init() first');
 
-    const response = await fetch(`${API_URL}${path}`, {
+    const response = await FETCH(`${API_URL}${path}`, {
         method,
         headers: {
             'Content-Type': 'application/json',
